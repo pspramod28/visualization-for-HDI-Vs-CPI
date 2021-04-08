34adf6ac2d42e0ec76594359bece0c636b7aa334
Srinivas A B <79242069+pspramod28@users.noreply.github.com>
2021-04-08 13:02:32 +0530

    Add files via upload

---
+++ HDI Vs CPI.R
@@ -0,0 +1,40 @@
+library(ggplot2)
+library(data.table)
+df <- read.csv("C:\\Users\\Sri\\Desktop\\Assignment\\Corroption Vs Human development\\Economist_Assignment_Data.csv")
+head(df)
+df$X=NULL 
+
+pl <- ggplot(df, aes(x=CPI, y=HDI, colour=Region)) +
+  geom_point(shape=1, size=2) 
+
+pl + geom_smooth(aes(group=1), fill=NA)
+
+pl2 <- pl + geom_smooth(aes(group=1),method ='lm',formula = y~log(x),se=FALSE,color='red')
+
+pl2 + geom_text(aes(label=Country))
+
+pointsToLabel <- c("Russia", "Venezuela", "Iraq", "Myanmar", "Sudan",
+                   "Afghanistan", "Congo", "Greece", "Argentina", "Brazil",
+                   "India", "Italy", "China", "South Africa", "Spane",
+                   "Botswana", "Cape Verde", "Bhutan", "Rwanda", "France",
+                   "United States", "Germany", "Britain", "Barbados", "Norway", "Japan",
+                   "New Zealand", "Singapore")
+
+pl3 <- pl2 + geom_text(aes(label = Country), color = "gray20", 
+                       data = subset(df, Country %in% pointsToLabel),check_overlap = TRUE)
+
+pl4 <- pl3 + theme_bw()
+
+pl5 <- pl4 + scale_x_continuous(name = "Corruption Perceptions Index, 2011 (10=least corrupt)",
+                                limits = c(.9, 10.5),breaks=1:10)
+
+pl6 <- pl5 + scale_y_continuous(name = "Human Development Index, 2011 (1=Best)",
+                                limits = c(0.2, 1.0))
+
+pl6 + ggtitle("Corruption and Human development")
+
+library(ggthemes)
+pl6 + theme_economist_white()
+
+library(plotly)
+ggplotly(pl6)
